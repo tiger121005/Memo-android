eddd8f6e2209634cc5aa33a360b9fbc78157456e
tiger121005 <tiger121005@github.com\nalkvjn>
2024-09-02 15:22:07 +0900

    level0

---
+++ app/src/main/java/app/ito/yomi/memo/CustomAdapter.kt
@@ -0,0 +1,23 @@
+package app.ito.yomi.memo
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+class CustomAdapter(context: Context, list: MutableList<String>): ArrayAdapter<String>(context, 0, list) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view  = convertView
+        if (view == null) {
+            // 一行分のレイアウトを生成
+            view  = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
+        }
+        // 一行分のデータを取得
+        val data = getItem(position) as String
+        view?.findViewById<TextView>(R.id.title)?.apply { text = data }
+        return view!!
+    }
+
+}
--- app/src/main/java/app/ito/yomi/memo/MainActivity.kt
+++ app/src/main/java/app/ito/yomi/memo/MainActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import app.ito.yomi.memo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var list: MutableList<String> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        updateList(list)
+
+        binding.addBtn.setOnClickListener {
+            list.add(binding.editText.text.toString())
+            binding.editText.setText("")
+            updateList(list)
         }
     }
+
+    fun updateList(data: MutableList<String>) {
+        val adapter = CustomAdapter(this, list)
+        binding.listview.adapter = adapter
+    }
+
 }
