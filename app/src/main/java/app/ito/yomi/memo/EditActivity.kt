c98ebfe7e2a16083867c5662a292fa2dff0b1ba6
tiger121005 <tiger121005@github.com\nalkvjn>
2024-09-04 17:46:28 +0900

    refactor edit

--- app/src/main/java/app/ito/yomi/memo/EditActivity.kt
+++ app/src/main/java/app/ito/yomi/memo/EditActivity.kt
@@ -10,27 +10,40 @@ class EditActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditBinding
     private val viewModel: MemoViewModel by viewModels()
 
+    private var id: Int = 0
+    private var title: String = ""
+    private var text: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityEditBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
-
-
-        val id = intent.getIntExtra("id", 0)
-        val title = intent.getStringExtra("title")  ?: ""
-        val text = intent.getStringExtra("text") ?: ""
-
-        binding.editTitle.setText(title)
-        binding.editText.setText(text)
+        getFromDetail()
+        setText()
 
         binding.saveBtn.setOnClickListener {
-            viewModel.update(MemoData(id = id, title = binding.editTitle.text.toString(), text = binding.editText.text.toString()))
-            finish()
+            tapSaveButton()
         }
 
 
+    }
+
+    fun getFromDetail() {
+        id = intent.getIntExtra("id", 0)
+        title = intent.getStringExtra("title")  ?: ""
+        text = intent.getStringExtra("text") ?: ""
+    }
+
+    fun setText() {
+        binding.editTitle.setText(title)
+        binding.editText.setText(text)
+    }
+
+    fun tapSaveButton() {
+        viewModel.update(MemoData(id = id, title = binding.editTitle.text.toString(), text = binding.editText.text.toString()))
+        finish()
     }
 
 
